f852aede87cf551c7325f6a9e5f0490c43ad75c5
tieri25 <36459858+tieri25@users.noreply.github.com>
2018-12-19 14:36:27 +0100

    organizzazione

---
+++ Arduino/Robot/Sensors.h
@@ -0,0 +1,22 @@
+#include <Wire.h>
+#include <Adafruit_TCS34725.h>
+
+#define TCAADDR 0x70
+#define CSLAddr 2
+#define CSRAddr 5
+#define CSCAddr 4
+
+const char COL_RED='r', COL_GREEN='g', COL_WHITE='w', COL_BLACK='b', COL_SILVER='s', COL_UNKNOWN='u';
+
+class Sensors {
+	public :
+		Sensors();
+		char getColorRight();
+		char getColorLeft();
+		char getColorCenter();
+	private :
+		Adafruit_TCS34725 colorSensorL, colorSensorR, colorSensorC;
+		void tcaSelect(uint8_t addr);
+		char getColorID(int colors[]);
+		char ReadColorSensor(Adafruit_TCS34725& colorSensor);
+};
