9361da6abd5e943ba5cfaca5d318209783d0b628
Marco Buccione <marco.buccione@istruzione.it>
2019-04-05 18:54:53 +0200

    Formattazione .ino

--- Arduino/robocup/robocup.ino
+++ Arduino/robocup/robocup.ino
@@ -33,42 +33,42 @@ void setup() {
   pinMode(FWD_LEFT, INPUT_PULLUP);
   pinMode(BACK_RIGHT, INPUT_PULLUP);
   pinMode(BACK_LEFT, INPUT_PULLUP);
-//  pinMode(SILVER, INPUT_PULLUP);
+  //  pinMode(SILVER, INPUT_PULLUP);
   Serial.begin(9600);
   Wire.begin(SLAVE_ADDRESS);
   Wire.onReceive(receiveData);
   Wire.onRequest(sendData);
   Serial.println("Ready!");
   // Per il test dei sensori di colore
-//  sensors->readAllColors();
-//  int blackLevel = sensors->detectBlack();
-//  Serial.println(blackLevel);
+  //  sensors->readAllColors();
+  //  int blackLevel = sensors->detectBlack();
+  //  Serial.println(blackLevel);
   Wire2.begin();
   mpu6050.begin();
   mpu6050.calcGyroOffsets(true);
 }
 
 void loop() {
-  if(rescueLineMode) {
+  if (rescueLineMode) {
     sensors->readAllColors();
-//    sensors->debugColors();
-//    if(!lSilver) {
-//      lSilver = digitalRead(SILVER); 
-//    }
-//    Serial.println(lSilver)  ;
+    //    sensors->debugColors();
+    //    if(!lSilver) {
+    //      lSilver = digitalRead(SILVER);
+    //    }
+    //    Serial.println(lSilver)  ;
   }
-  
+
   dist = ultra.ping_cm();
-  
+
   mpu6050.update();
-  if(millis() - timer > 1000){
-     gyroX = (int) mpu6050.getAngleX();
-     gyroY = (int) mpu6050.getAngleY();
-     gyroZ = (int) mpu6050.getAngleZ();
-//     Serial.print("angleX : ");Serial.print(gyroX);
-//     Serial.print("\tangleY : ");Serial.print(gyroY);
-//     Serial.print("\tangleZ : ");Serial.println(gyroZ);
-     timer = millis();
+  if (millis() - timer > 1000) {
+    gyroX = (int) mpu6050.getAngleX();
+    gyroY = (int) mpu6050.getAngleY();
+    gyroZ = (int) mpu6050.getAngleZ();
+    //     Serial.print("angleX : ");Serial.print(gyroX);
+    //     Serial.print("\tangleY : ");Serial.print(gyroY);
+    //     Serial.print("\tangleZ : ");Serial.println(gyroZ);
+    timer = millis();
   }
 }
 
@@ -79,22 +79,22 @@ void receiveData(int byteCount) {
 }
 
 /*
- * Comandi per il dialogo con Arduino:
- * B = rilevazione del livello del nero
- * C = checkColors: lux + colori di tutti i sensori
- * D = misurazione distanza con sensore a ultrasuoni
- * G = dati giroscopio 
- * T = tutti i 4 sensori di tocco
- * S = controllo argento (disattivato)
- * Z = attiva modalità zona vittime
- * L = attiva modalità seguilinea
- */
+   Comandi per il dialogo con Arduino:
+   B = rilevazione del livello del nero
+   C = checkColors: lux + colori di tutti i sensori
+   D = misurazione distanza con sensore a ultrasuoni
+   G = dati giroscopio
+   T = tutti i 4 sensori di tocco
+   S = controllo argento (disattivato)
+   Z = attiva modalità zona vittime
+   L = attiva modalità seguilinea
+*/
 void sendData() {
-  if(request == 'B') {
+  if (request == 'B') {
     int blackLevel = sensors->detectBlack();
     Wire.write(lowByte(blackLevel));
     Wire.write(highByte(blackLevel));
-  } else if(request == 'C') {
+  } else if (request == 'C') {
     Wire.write(lowByte(sensors->getLuxLeft()));
     Wire.write(highByte(sensors->getLuxLeft()));
     Wire.write(lowByte(sensors->getLuxCenter()));
@@ -104,57 +104,57 @@ void sendData() {
     Wire.write(sensors->getColorLeft());
     Wire.write(sensors->getColorCenter());
     Wire.write(sensors->getColorRight());
-  } else if(request == 'D') {
-//     Serial.println(dist);
-     Wire.write(lowByte(dist));
-     Wire.write(highByte(dist));
-  } else if(request == 'G') {
+  } else if (request == 'D') {
+    //     Serial.println(dist);
+    Wire.write(lowByte(dist));
+    Wire.write(highByte(dist));
+  } else if (request == 'G') {
     Wire.write(lowByte(gyroX));
     Wire.write(highByte(gyroX));
     Wire.write(lowByte(gyroY));
     Wire.write(highByte(gyroY));
     Wire.write(lowByte(gyroZ));
     Wire.write(highByte(gyroZ));
-  } else if(request == 'T') {
-    if(digitalRead(FWD_RIGHT) == HIGH) {
-//      Serial.println("Anteriore Destro premuto");
+  } else if (request == 'T') {
+    if (digitalRead(FWD_RIGHT) == HIGH) {
+      //      Serial.println("Anteriore Destro premuto");
       Wire.write(true);
     } else {
       Wire.write(false);
     }
-    if(digitalRead(FWD_LEFT) == HIGH) {
-//      Serial.println("Anteriore Sinistro premuto");
+    if (digitalRead(FWD_LEFT) == HIGH) {
+      //      Serial.println("Anteriore Sinistro premuto");
       Wire.write(true);
     } else {
       Wire.write(false);
     }
-    if(digitalRead(BACK_RIGHT) == HIGH) {
-//      Serial.println("Posteriore Destro premuto");
+    if (digitalRead(BACK_RIGHT) == HIGH) {
+      //      Serial.println("Posteriore Destro premuto");
       Wire.write(true);
     } else {
       Wire.write(false);
     }
-    if(digitalRead(BACK_LEFT) == HIGH) {
-//      Serial.println("Posteriore Sinistro premuto");
+    if (digitalRead(BACK_LEFT) == HIGH) {
+      //      Serial.println("Posteriore Sinistro premuto");
       Wire.write(true);
     } else {
       Wire.write(false);
     }
-//  } else if(request == 'S') {
-//    if (lSilver == LOW) {
-//      //Serial.println("Argento Trovato");
-//      Wire.write(true);
-//    } else {
-//      Wire.write(false);
-//    }
-  } else if(request == 'Z') { 
+    //  } else if(request == 'S') {
+    //    if (lSilver == LOW) {
+    //      //Serial.println("Argento Trovato");
+    //      Wire.write(true);
+    //    } else {
+    //      Wire.write(false);
+    //    }
+  } else if (request == 'Z') {
     // Imposto la modalità zona vittime
     rescueLineMode = false;
     Wire.write(true);
-  } else if(request == 'L') {
+  } else if (request == 'L') {
     // Imposto la modalità seguilinea
     rescueLineMode = true;
-//    lSilver = false;
+    //    lSilver = false;
     Wire.write(true);
   }
 }
